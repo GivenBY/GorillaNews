cdfe082541cfcea23f387fb7c17fb63a369a1ca6
GivenBY <suryakantupadhyay18@gmail.com>
2024-02-20 18:30:01 +0530

    fix: deploy errors

--- src/components/News.js
+++ src/components/News.js
@@ -5,7 +5,7 @@ import Spinner from "./Spinner";
 import PropTypes from "prop-types";
 import InfiniteScroll from "react-infinite-scroll-component";
 
-const News = (props) => {
+const News = ({ country, category, apiKey, pageSize, setProgress }) => {
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(true);
   const [page, setPage] = useState(1);
@@ -16,37 +16,28 @@ const News = (props) => {
   };
 
   const updateNews = useCallback(async () => {
-    props.setProgress(10);
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+    setProgress(10);
+    const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${apiKey}&page=${page}&pageSize=${pageSize}`;
     setLoading(true);
     let data = await fetch(url);
-    props.setProgress(30);
+    setProgress(30);
     let parsedData = await data.json();
-    props.setProgress(70);
+    setProgress(70);
     setArticles(parsedData.articles);
     setTotalResults(parsedData.totalResults);
     setLoading(false);
-    props.setProgress(100);
-  }, [
-    page,
-    props.country,
-    props.category,
-    props.apiKey,
-    props.pageSize,
-    props.setProgress,
-  ]);
+    setProgress(100);
+  }, [country, category, apiKey, pageSize, setProgress, page]);
 
   useEffect(() => {
-    document.title = `${capitalizeFirstLetter(props.category)} - GorillaNews`;
+    document.title = `${capitalizeFirstLetter(category)} - GorillaNews`;
     updateNews();
-  }, [props.category, updateNews]);
+  }, [category, updateNews]);
 
   const fetchMoreData = async () => {
-    const url = `https://newsapi.org/v2/top-headlines?country=${
-      props.country
-    }&category=${props.category}&apiKey=${props.apiKey}&page=${
+    const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${apiKey}&page=${
       page + 1
-    }&pageSize=${props.pageSize}`;
+    }&pageSize=${pageSize}`;
     setPage(page + 1);
     let data = await fetch(url);
     let parsedData = await data.json();
@@ -59,7 +50,7 @@ const News = (props) => {
         className="text-center"
         style={{ margin: "35px 0px", marginTop: "90px", textAlign: "center" }}
       >
-        GorillaNews - Top {capitalizeFirstLetter(props.category)} Headlines
+        GorillaNews - Top {capitalizeFirstLetter(category)} Headlines
       </h1>
       {loading && <Spinner />}
       <InfiniteScroll
